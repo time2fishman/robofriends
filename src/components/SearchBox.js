7e24a8cf6a0628c22e019fa5127f763e6037085c
time2fishman <adam_h_martinez@hotmail.com>
2023-11-20 13:10:24 -0700

    remove unused prop

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const SearchBox = ({searchField, searchChange}) => {
+const SearchBox = ({ searchChange }) => {
   return (
     <div className='pa2'>
-      <input 
+      <input
         className='pa3 ba b--green bg-lightest-blue'
-        type='search' 
-        placeholder='search robots' 
+        type='search'
+        placeholder='search robots'
         onChange={searchChange}
       />
     </div>
