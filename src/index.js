a5797933b70aa8e7d8041da094dabd3067d18b52
time2fishman <adam_h_martinez@hotmail.com>
2023-11-21 09:41:07 -0700

    added redux middleware for logging

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore} from 'redux'
+import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App'
 import reportWebVitals from './reportWebVitals';
 import { searchRobots } from './reducers';
 import 'tachyons'
 
-const store = createStore(searchRobots)
+const logger = createLogger()
+const store = createStore(searchRobots, applyMiddleware(logger))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
